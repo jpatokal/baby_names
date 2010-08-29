82eca44870c9b138e570f4d2002221340022b6e0
Jani Patokallio <jpatokal@iki.fi>
2010-08-29 12:18:45 +1000

    Basic routing

--- config/routes.rb
+++ config/routes.rb
@@ -35,7 +35,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  map.root :controller => "home"
 
   # See how all your routes lay out with "rake routes"
 
@@ -44,4 +44,5 @@ ActionController::Routing::Routes.draw do |map|
   # consider removing or commenting them out if you're using named routes and resources.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
+
 end
