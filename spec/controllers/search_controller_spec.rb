ca9906ce2f07ee75d430cac6486ef46773453619
Jani Patokallio <jpatokal@iki.fi>
2010-09-04 14:34:55 +1000

    Test gender exclusion

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -1,21 +1,31 @@
 require 'spec_helper'
 
 describe SearchController do
-  describe "if two valid languages are given" do
+  describe "if two valid languages and matching names are given" do
     before do
       @lang1 = Language.create(:english => 'Foo 1', :local => 'Bar 1')
-      @name1 = @lang1.names.create(:latin => 'Name', :normalized => 'Match')
+      @name1 = @lang1.names.create(:latin => 'Name', :normalized => 'Match', :gender => 'M')
       @name1.save!
+      @red_herring = @lang1.names.create(:latin => 'Wrong gender', :normalized => 'Match', :gender => 'F')
+      @red_herring.save!
 
       @lang2 = Language.create(:english => 'Foo 2', :local => 'Bar 2')
-      @name2 = @lang2.names.create(:latin => 'Name', :normalized => 'Match')
+      @name2 = @lang2.names.create(:latin => 'Name', :normalized => 'Match', :gender => 'M')
       @name2.save!
-    end
-    
-    it "should get list of matches" do
+
       get :search, :language => {:first => @lang1.id, :second => @lang2.id }
-      assigns[:results][0].should include(@name1, @name2)
+    end
+
+    it "should return success" do
       response.should be_success
+    end
+
+    it "should get list of matches" do
+      assigns[:results].flatten.should include(@name1, @name2)
+    end
+
+    it "should exclude different genders" do
+      assigns[:results].flatten.should_not include(@red_herring)
     end
   end
 end
